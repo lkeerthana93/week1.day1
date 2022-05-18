70039d9328106b7f9f5a0e5f2852ad352d4fe127
lkeerthana93 <50731446+lkeerthana93@users.noreply.github.com>
2022-05-19 00:41:33 +0530

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,29 @@
+package week1.day1;
+
+public class Car {
+	public void applyBreak()
+	{
+		System.out.println("Apply break method has been called");
+	}
+	public void applyGear()
+	{
+		System.out.println("Apply gear method has been called");
+	}
+	public void switchonAc()
+	{
+		System.out.println("Switch On method has been called");
+	}
+	public void applyAcclerate()
+	{
+		System.out.println("Apply accelerate method has been called");
+	}
+
+	public static void main(String[] args) {
+		Car c = new Car();
+		c.applyAcclerate();
+		c.applyBreak();
+		c.applyGear();
+		c.switchonAc();
+		
+	}
+}
---
+++ MyCar.java
@@ -0,0 +1,13 @@
+package week1.day1;
+
+public class MyCar {
+		public void main(String[] args) {
+			Car c = new Car();
+			c.applyAcclerate();
+			c.applyBreak();
+			c.applyGear();
+			c.switchonAc();
+			
+		}		
+		
+}
